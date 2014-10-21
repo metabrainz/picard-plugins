2cfa88402102c7d60eca88e0d1c4b8de89b2a5bf
Philipp Wolfer <ph.wolfer@gmail.com>
2014-10-21 18:07:10 +0200

    Added support for the hachoir module for basic metadata parsing.
    Now supports MPEG, QuickTime, Matroska and AVI files.

---
+++ plugins/videotools/__init__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2014 Philipp Wolfer
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+
+PLUGIN_NAME = 'Video tools'
+PLUGIN_AUTHOR = 'Philipp Wolfer'
+PLUGIN_DESCRIPTION = 'Improves the video support in Picard by adding support for Matroska, AVI, QuickTime and MPEG files (renaming and fingerprinting only, no tagging) and providing $is_audio() and $is_video() scripting functions. Requires the hachoir Python module for full functionality.'
+PLUGIN_VERSION = "0.1"
+PLUGIN_API_VERSIONS = ["1.0.0"]
+
+from picard.formats import register_format
+from picard.script import register_script_function
+from picard.plugins.videotools.hachoir import AviFile, MatroskaFile, QuickTimeFile
+from picard.plugins.videotools.mpeg import MpegFile
+from picard.plugins.videotools.script import is_audio, is_video
+
+
+register_format(AviFile)
+register_format(MatroskaFile)
+register_format(MpegFile)
+register_format(QuickTimeFile)
+
+register_script_function(is_audio)
+register_script_function(is_video)
