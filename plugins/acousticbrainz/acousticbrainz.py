dca599d6772a69165a6165c485984744ba08f93d
Andrew Cook <ariscop@gmail.com>
2014-11-17 13:50:30 +1100

    Now works, assigns moods and genres

--- plugins/acousticbrainz/acousticbrainz.py
+++ plugins/acousticbrainz/acousticbrainz.py
@@ -6,18 +6,33 @@ PLUGIN_DESCRIPTION = u'''Uses Acousticbrainz for mood and genre'''
 PLUGIN_VERSION = "0.0"
 PLUGIN_API_VERSIONS = ["0.15"]
 
+from json import loads
 from functools import partial
+from picard import log
 from picard.metadata import register_track_metadata_processor
 
 ACOUSTICBRAINZ_HOST = "acousticbrainz.org"
 ACOUSTICBRAINZ_PORT = 80
 
 def result(album, metadata, release, track, data, reply, error):
-    album._requests -= 1
-    album._finalize_loading(None)
+    moods = []
+    genres = []
+    bpm = False
+    try:
+        data = loads(data)["highlevel"]
+        for k, v in data.items():
+            if k.startswith("genre_") and not v["value"].startswith("not_"):
+                genres.append(v["value"])
+            if k.startswith("mood_") and not v["value"].startswith("not_"):
+                moods.append(v["value"])
+        metadata["genre"] = ", ".join(genres)
+        metadata["moods"] = ", ".join(moods)
+    finally:
+        album._requests -= 1
+        album._finalize_loading(None)
 
 def process_track(album, metadata, release, track):
-    album.tagger.xmlws.get(
+    album.tagger.xmlws.download(
         ACOUSTICBRAINZ_HOST,
         ACOUSTICBRAINZ_PORT,
         u"/%s/high-level" % (metadata["musicbrainz_recordingid"]),
