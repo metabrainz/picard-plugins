6b5f7edb4349301c9927b2ddfdb9ef6dc3556b64
NCenerar <ncenerar@free.fr>
2015-01-30 13:28:22 +0100

    Adding license

--- plugins/save_and_rewrite_header/save_and_rewrite_header.py
+++ plugins/save_and_rewrite_header/save_and_rewrite_header.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+#
+# This source code is a plugin for MusicBrainz Picard.
+# It adds a context menu action to save files and rewrite their header.
+# Copyright (C) 2015 Nicolas Cenerario
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/gpl-3.0.txt>.
+
 from __future__ import unicode_literals
 
 PLUGIN_NAME = "Save and rewrite header"
@@ -7,6 +25,8 @@ PLUGIN_AUTHOR = "Nicolas Cenerario"
 PLUGIN_DESCRIPTION = "This plugin adds a context menu action to save files and rewrite their header."
 PLUGIN_VERSION = "0.1"
 PLUGIN_API_VERSIONS = ["0.9.0", "0.10", "0.15"]
+PLUGIN_LICENSE = "GPL-3.0"
+PLUGIN_LICENSE_URL = "http://www.gnu.org/licenses/gpl-3.0.txt"
 
 from _functools import partial
 from mutagen import File as MFile
