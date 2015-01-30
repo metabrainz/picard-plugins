6391ba202385608f0d8e9a0caff41ba25d93de30
Sophist <github@sodalis.co.uk>
2015-01-30 12:47:15 +0000

    Add GPL license details to sort_multivalue_tags.py

--- plugins/sort_multivalue_tags/sort_multivalue_tags.py
+++ plugins/sort_multivalue_tags/sort_multivalue_tags.py
@@ -18,6 +18,8 @@ PLUGIN_AUTHOR = u"Sophist"
 PLUGIN_DESCRIPTION = u'Sort Multi-Value Tags e.g. Release Type, Lyrics alphabetically.'
 PLUGIN_VERSION = "0.1"
 PLUGIN_API_VERSIONS = ["0.15"]
+PLUGIN_LICENSE = "GNU General Public License v3"
+PLUGIN_LICENSE_URL = "http://www.gnu.org/licenses/gpl.html"
 
 from picard.metadata import register_track_metadata_processor
 
