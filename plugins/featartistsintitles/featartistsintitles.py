c71ee42a390638487fa4b0689eaabdf53eacd92f
GitrianDK <GitrianDK@users.noreply.github.com>
2016-02-14 00:54:40 +0100

    Update featartistsintitles.py
    Made a list on which words to move from artist names to album and track titles, so it't not only "feat.". Also made an ignorelist. 
    Would be better if it was in a configuration file, but as this is my first try in Python it must come in a later version :-)

--- plugins/featartistsintitles/featartistsintitles.py
+++ plugins/featartistsintitles/featartistsintitles.py
@@ -1,27 +1,36 @@
 PLUGIN_NAME = 'Feat. Artists in Titles'
-PLUGIN_AUTHOR = 'Lukas Lalinsky, Michael Wiencek, Bryan Toth'
-PLUGIN_DESCRIPTION = 'Move "feat." from artist names to album and track titles. Match is case insensitive.'
-PLUGIN_VERSION = "0.3"
+PLUGIN_AUTHOR = 'Lukas Lalinsky, Michael Wiencek, Bryan Toth, Brian Altenburg'
+PLUGIN_DESCRIPTION = 'Move "feat." and other from artist names to album and track titles. Match is case insensitive.'
+PLUGIN_VERSION = "0.4"
 PLUGIN_API_VERSIONS = ["0.9.0", "0.10", "0.15", "0.16"]
 
 from picard.metadata import register_album_metadata_processor, register_track_metadata_processor
-import re
+import re 
 
-_feat_re = re.compile(r"([\s\S]+) feat\.([\s\S]+)", re.IGNORECASE)
-
+list = ['feat.', 'with', 'duet med', 'med']
+ignoreList = ['danser med drenge']
 
 def move_album_featartists(tagger, metadata, release):
-    match = _feat_re.match(metadata["albumartist"])
-    if match:
-        metadata["albumartist"] = match.group(1)
-        metadata["album"] += " (feat.%s)" % match.group(2)
-
+	for item in list:
+		for item2 in ignoreList:
+			if item2.find(item) > -1:
+				break
+			_item_re = re.compile(r"([\s\S]+) "+item+" ([\s\S]+)", re.IGNORECASE)
+			match = _item_re.match(metadata["albumartist"])
+			if match:
+				metadata["albumartist"] = match.group(1)
+				metadata["album"] += " ("+item+" %s)" % match.group(2)
 
 def move_track_featartists(tagger, metadata, release, track):
-    match = _feat_re.match(metadata["artist"])
-    if match:
-        metadata["artist"] = match.group(1)
-        metadata["title"] += " (feat.%s)" % match.group(2)
+	for item in list:
+		for item2 in ignoreList:
+			if item2.find(item) > -1:
+				break
+			_item_re = re.compile(r"([\s\S]+) "+item+" ([\s\S]+)", re.IGNORECASE)
+			match = _item_re.match(metadata["artist"])
+			if match:
+				metadata["artist"] = match.group(1)
+				metadata["title"] += " ("+item+" %s)" % match.group(2)
 
 register_album_metadata_processor(move_album_featartists)
 register_track_metadata_processor(move_track_featartists)
