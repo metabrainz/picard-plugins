c4cf52a60a237d085dafc0a7cb9bf48b4416e59a
Francis Chin <dev@fchin.com>
2016-08-08 20:52:16 +0100

    Version 0.2: Various Artists, source code licence
    Feature: suppress "Various Artists" in default playlist filename.
    Added missing source code licence.
    Added logging and debug.

--- plugins/playlist/playlist.py
+++ plugins/playlist/playlist.py
@@ -1,19 +1,36 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
 PLUGIN_NAME = u"Generate M3U playlist"
 PLUGIN_AUTHOR = u"Francis Chin"
-PLUGIN_DESCRIPTION = u"Generate an Extended M3U playlist (.m3u8 utf_8 \
-encoded file). Relative pathnames are used if saving to the directory \
-containing the audio files, otherwise absolute pathnames are used."
-PLUGIN_VERSION = "0.1"
-PLUGIN_API_VERSIONS = ["0.10", "0.15"]
+PLUGIN_DESCRIPTION = u"""Generate an Extended M3U playlist (.m3u8 file,
+UTF8 encoded text). Relative pathnames are used if saving to the directory
+that contains the audio files, otherwise absolute pathnames are used."""
+PLUGIN_VERSION = "0.2"
+PLUGIN_API_VERSIONS = ["1.0.0", "1.1.0", "1.2.0", "1.3.0"]
+PLUGIN_LICENSE = "GPL-2.0"
+PLUGIN_LICENSE_URL = "https://www.gnu.org/licenses/gpl-2.0.html"
 
 import os.path
 
 from PyQt4 import QtCore, QtGui
+from picard import log
+from picard.const import VARIOUS_ARTISTS_ID
 from picard.util import find_existing_path, encode_filename
 from picard.ui.itemviews import BaseAction, register_album_action
+
+
+_debug_level = 0
 
 
 class PlaylistEntry(list):
@@ -55,10 +72,17 @@ class GeneratePlaylist(BaseAction):
         current_directory = (self.config.persist["current_directory"]
                              or QtCore.QDir.homePath())
         current_directory = find_existing_path(unicode(current_directory))
-        # Default playlist filename set as "%albumartist% - %album%.m3u8"
-        # To do: make configurable
-        default_filename = (objs[0].metadata["albumartist"] + " - "
-                            + objs[0].metadata["album"] + ".m3u8")
+        # Default playlist filename set as "%albumartist% - %album%.m3u8",
+        # except where "Various Artists" is suppressed
+        if _debug_level > 1:
+            log.debug("{}: VARIOUS_ARTISTS_ID is {}, musicbrainz_albumartistid is {}".format(
+                    PLUGIN_NAME, VARIOUS_ARTISTS_ID,
+                    objs[0].metadata["musicbrainz_albumartistid"]))
+        if objs[0].metadata["musicbrainz_albumartistid"] != VARIOUS_ARTISTS_ID:
+            default_filename = (objs[0].metadata["albumartist"] + " - "
+                                + objs[0].metadata["album"] + ".m3u8")
+        else:
+            default_filename = objs[0].metadata["album"] + ".m3u8"
         b_filename, b_selected_format = QtGui.QFileDialog.getSaveFileNameAndFilter(
             None, "Save new playlist",
             os.path.join(current_directory, default_filename),
@@ -85,10 +109,17 @@ class GeneratePlaylist(BaseAction):
                             )
                         )
                         
-                        # M3U media file row - assumes only one file per track
+                        # M3U URL row - assumes only one file per track
                         audio_filename = track.linked_files[0].filename
+                        if _debug_level > 1:
+                            for i, file in enumerate(track.linked_files):
+                                log.debug("{}: linked_file {}: {}".format(
+                                    PLUGIN_NAME, i, str(file)))
                         # If playlist is in same directory as audio files, then use
                         # local (relative) pathname, otherwise use absolute pathname
+                        if _debug_level > 1:
+                            log.debug("{}: audio_filename: {}, selected dir: {}".format(
+                                    PLUGIN_NAME, audio_filename, os.path.dirname(filename)))
                         if os.path.dirname(filename) == os.path.dirname(audio_filename):
                             audio_filename = os.path.basename(audio_filename)
                         entry.add(unicode(audio_filename))
