acfba0f760e335192a2cadde907bca6b4b2c3c15
Daniel Sobey <dns@dns.id.au>
2016-10-30 16:50:41 +1030

    adding tracking of calls

--- plugins/wikidata/wikiata.py
+++ plugins/wikidata/wikiata.py
@@ -13,6 +13,8 @@ class wikidata:
     def process(self,tagger, metadata, release):
         self.xmlws=tagger.tagger.xmlws
         self.log=tagger.log
+        self.tagger=tagger
+        tagger._requests += 1
         release_id = dict.get(metadata,'musicbrainz_releasegroupid')[0]
         # find the wikidata url if this exists
         host = config.setting["server_host"]
@@ -20,11 +22,11 @@ class wikidata:
         path = '/ws/2/release-group/%s?inc=url-rels' % release_id
         
         self.xmlws.get(host, port, path,
-                       partial(self.website_process, release_id,metadata),
+                       partial(self.website_process, release_id,metadata,tagger),
                                 xml=True, priority=False, important=False)
-
     
-    def website_process(self,release_id,metadata, response, reply, error):
+    def website_process(self,release_id,metadata,tagger, response, reply, error):
+        found=False;
         if error:
             log.info('WIKIDATA: error retrieving release group info')
         else:
@@ -33,16 +35,24 @@ class wikidata:
                     if 'relation_list' in response.metadata[0].release_group[0].children:
                         for relation in response.metadata[0].release_group[0].relation_list[0].relation:
                             if relation.type == 'wikidata' and 'target' in relation.children:
+                                found=True
                                 wikidata_url=relation.target[0].text
-                                self.process_wikidata(wikidata_url,metadata)
-    def process_wikidata(self,wikidata_url,metadata):
+                                self.process_wikidata(wikidata_url,metadata,tagger)
+        if not found:
+            log.info('WIKIDATA: no wikidata url')
+            tagger._requests -= 1
+            if tagger._requests==0:
+                tagger._finalize_loading(None)
+
+
+    def process_wikidata(self,wikidata_url,metadata,tagger):
         item=wikidata_url.split('/')[4]
         path="/wiki/Special:EntityData/"+item+".rdf"
         log.info('WIKIDATA: fetching the folowing url wikidata.org%s' % path)
         self.xmlws.get('www.wikidata.org', 443, path,
-                       partial(self.parse_wikidata_response, item,metadata),
+                       partial(self.parse_wikidata_response, item,metadata,tagger),
                                 xml=True, priority=False, important=False)
-    def parse_wikidata_response(self,item,metadata, response, reply, error):
+    def parse_wikidata_response(self,item,metadata,tagger, response, reply, error):
         genre_entries=[]
         genre_list=[]
         if error:
@@ -77,6 +87,9 @@ class wikidata:
             metadata["genre"] = genre_list
         else:
             log.info('WIKIDATA: Genre not found in wikidata')
+        tagger._requests -= 1
+        if tagger._requests==0:
+            tagger._finalize_loading(None)
 
 register_album_metadata_processor(wikidata().process)
 
