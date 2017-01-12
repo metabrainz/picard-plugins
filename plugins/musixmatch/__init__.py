d71c3844095fb455c87ab0d0b58ade0cfb27b4ee
Ämin Baumeler <amin@zitune.ch>
2017-01-12 22:30:35 +0100

    applied autopep8 to the files

--- plugins/musixmatch/__init__.py
+++ plugins/musixmatch/__init__.py
@@ -10,45 +10,50 @@ from picard.ui.options import register_options_page, OptionsPage
 from picard.config import TextOption
 from ui_options_musixmatch import Ui_MusixmatchOptionsPage
 
+
 class MusixmatchOptionsPage(OptionsPage):
-	NAME = 'musixmatch'
-	TITLE = 'Musixmatch API Key'
-	PARENT = "plugins"
-	options = [
-			TextOption("setting","musixmatch_api_key","")
-	]
-	def __init__(self,parent=None):
-		super(MusixmatchOptionsPage,self).__init__(parent)
-		self.ui = Ui_MusixmatchOptionsPage()
-		self.ui.setupUi(self)
-	
-	def load(self):
-		self.ui.api_key.setText(self.config.setting["musixmatch_api_key"])
-	
-	def save(self):
-		self.config.setting["musixmatch_api_key"] = self.ui.api_key.text()
+    NAME = 'musixmatch'
+    TITLE = 'Musixmatch API Key'
+    PARENT = "plugins"
+    options = [
+        TextOption("setting", "musixmatch_api_key", "")
+    ]
+
+    def __init__(self, parent=None):
+        super(MusixmatchOptionsPage, self).__init__(parent)
+        self.ui = Ui_MusixmatchOptionsPage()
+        self.ui.setupUi(self)
+
+    def load(self):
+        self.ui.api_key.setText(self.config.setting["musixmatch_api_key"])
+
+    def save(self):
+        self.config.setting["musixmatch_api_key"] = self.ui.api_key.text()
 register_options_page(MusixmatchOptionsPage)
 
 import picard.tagger as tagger
 import os
 try:
-	os.environ['MUSIXMATCH_API_KEY'] = tagger.config.setting["musixmatch_api_key"]
+    os.environ['MUSIXMATCH_API_KEY'] = tagger.config.setting[
+        "musixmatch_api_key"]
 except:
-	pass
+    pass
 
 from musixmatch import track as TRACK
 
+
 def process_track(album, metadata, release, track):
-	if('MUSIXMATCH_API_KEY' not in os.environ):
-		return
-	try:
-		t = TRACK.Track(metadata['musicbrainz_trackid'],musicbrainz=True).lyrics()
-		if t['instrumental'] == 1:
-			lyrics = "[Instrumental]"
-		else:
-			lyrics = t['lyrics_body']
-		metadata['lyrics:description'] = lyrics
-	except Exception as e:
-		pass
+    if('MUSIXMATCH_API_KEY' not in os.environ):
+        return
+    try:
+        t = TRACK.Track(metadata['musicbrainz_trackid'],
+                        musicbrainz=True).lyrics()
+        if t['instrumental'] == 1:
+            lyrics = "[Instrumental]"
+        else:
+            lyrics = t['lyrics_body']
+        metadata['lyrics:description'] = lyrics
+    except Exception as e:
+        pass
 
 register_track_metadata_processor(process_track)
