2b6584a91e5d314f7ff682ee096adb13f2fa1840
Ämin Baumeler <amin@zitune.ch>
2017-01-12 23:03:07 +0100

    license details added

--- plugins/musixmatch/__init__.py
+++ plugins/musixmatch/__init__.py
@@ -3,6 +3,8 @@ PLUGIN_AUTHOR = 'm-yn'
 PLUGIN_DESCRIPTION = 'Fetch first 30% of lyrics from Musixmatch'
 PLUGIN_VERSION = '0.2'
 PLUGIN_API_VERSIONS = ["0.9.0", "0.10", "0.15"]
+PLUGIN_LICENSE = "GPL-2.0"
+PLUGIN_LICENSE_URL = "https://www.gnu.org/licenses/gpl-2.0.html"
 
 
 from picard.metadata import register_track_metadata_processor
