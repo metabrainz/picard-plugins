d80625dee615cf4cf56e53a99629a900aaec1ee7
Sophist <github@sodalis.co.uk>
2017-01-18 20:33:18 +0000

    Add Remove Perfect Albums plugin
    Code taken from https://community.metabrainz.org/t/how-to-update-a-large-collection/32317/12
    
    I have assumed authors would be willing to have GPL2 license added.

---
+++ plugins/remove_perfect_albums/remove_perfect_albums.py
@@ -0,0 +1,22 @@
+PLUGIN_NAME = u'Remove Perfect Albums'
+PLUGIN_AUTHOR = u'ichneumon, hrglgrmpf'
+PLUGIN_DESCRIPTION = u'''Remove all perfectly matched albums from the selection.'''
+PLUGIN_VERSION = '0.2'
+PLUGIN_API_VERSIONS = ['0.15']
+PLUGIN_LICENSE = "GPL-2.0"
+PLUGIN_LICENSE_URL = "https://www.gnu.org/licenses/gpl-2.0.html"
+
+from picard.album import Album
+from picard.ui.itemviews import BaseAction, register_album_action
+
+class RemovePerfectAlbums(BaseAction):
+    NAME = 'Remove perfect albums'
+
+    def callback(self, objs):
+        for album in objs:
+            if isinstance(album, Album) and album.is_complete() and album.get_num_unmatched_files() == 0\
+              and album.get_num_matched_tracks() == len(list(album.iterfiles()))\
+              and album.get_num_unsaved_files() == 0 and album.loaded == True:
+                self.tagger.remove_album(album)
+
+register_album_action(RemovePerfectAlbums())
