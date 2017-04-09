54bb8ff51972bb785b1750feced842bc58e3ca9c
Sambhav Kothari <sambhavs.email@gmail.com>
2017-04-09 14:45:03 +0530

    Fix tests and add cleanup

--- test.py
+++ test.py
@@ -2,8 +2,9 @@ from __future__ import print_function
 import os
 import glob
 import sys
+import shutil
+import tempfile
 import unittest
-
 # python 2 & 3 compatibility
 try:
     basestring
@@ -12,11 +13,6 @@ except NameError:
 
 from generate import *
 
-# The file that contains json data
-plugin_file = "plugins.json"
-
-# The directory which contains plugin files
-plugin_dir = "plugins"
 
 if sys.version_info[:2] == (2, 6):
     def assertIsInstance(self, obj, cls, msg=None):
@@ -30,6 +26,19 @@ class GenerateTestCase(unittest.TestCase):
 
     """Run tests"""
 
+    # The file that contains json data
+    PLUGIN_FILE = "plugins.json"
+
+    # The directory which contains plugin files
+    PLUGIN_DIR = "plugins"
+
+    def setUp(self):
+
+        # Destination directory
+        self.dest_dir = tempfile.mkdtemp()
+        self.addCleanup(shutil.rmtree, self.dest_dir)
+        self.plugin_file = os.path.join(self.dest_dir, self.PLUGIN_FILE)
+
     def test_generate_json(self):
         """
         Generates the json data from all the plugins
@@ -38,14 +47,14 @@ class GenerateTestCase(unittest.TestCase):
 
         print("\n#########################################\n")
 
-        build_json()
+        build_json(self.dest_dir)
 
         # Load the json file
-        with open(plugin_file, "r") as in_file:
+        with open(self.plugin_file, "r") as in_file:
             plugin_json = json.load(in_file)["plugins"]
 
         # All top level directories in plugin_dir
-        plugin_folders = next(os.walk(plugin_dir))[1]
+        plugin_folders = next(os.walk(self.PLUGIN_DIR))[1]
 
         # Number of entries in the json should be equal to the
         # number of folders in plugin_dir
@@ -59,10 +68,10 @@ class GenerateTestCase(unittest.TestCase):
 
         print("\n\n#########################################\n")
 
-        zip_files()
+        zip_files(self.dest_dir)
 
         # All zip files in plugin_dir
-        plugin_zips = glob.glob(os.path.join(plugin_dir, "*.zip"))
+        plugin_zips = glob.glob(os.path.join(self.dest_dir, "*.zip"))
 
         # All top level directories in plugin_dir
         plugin_folders = next(os.walk(plugin_dir))[1]
@@ -78,10 +87,10 @@ class GenerateTestCase(unittest.TestCase):
 
         print("\n#########################################\n")
 
-        build_json()
+        build_json(self.dest_dir)
 
         # Load the json file
-        with open(plugin_file, "r") as in_file:
+        with open(self.plugin_file, "r") as in_file:
             plugin_json = json.load(in_file)["plugins"]
 
         # All plugins should contain all required fields
@@ -91,6 +100,7 @@ class GenerateTestCase(unittest.TestCase):
             self.assertIsInstance(data['author'], basestring)
             self.assertIsInstance(data['description'], basestring)
             self.assertIsInstance(data['version'], basestring)
+
 
 if __name__ == '__main__':
     unittest.main()
