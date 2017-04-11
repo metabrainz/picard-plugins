baf8cfeb5b1832b0902d465252c2f1c1a29667e3
Sophist <github@sodalis.co.uk>
2017-04-11 16:50:42 +0100

    Correct introductory blurb inc copyright.

--- plugins/smart_title_case/smart_title_case.py
+++ plugins/smart_title_case/smart_title_case.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
+# This is the Smart Title Case plugin for MusicBrainz Picard.
+# Copyright (C) 2017 Sophist.
+#
+# It is based on the Title Case plugin by Javier Kohen
 # Copyright 2007 Javier Kohen
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License version 2 as
-# published by the Free Software Foundation
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
 PLUGIN_NAME = "Smart Title Case"
 PLUGIN_AUTHOR = u"Sophist based on an earlier plugin by Javier Kohen"
