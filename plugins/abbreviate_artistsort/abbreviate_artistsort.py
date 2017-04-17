c66c7e2d54f1ecbf50a4e18290d60659ac8b365a
Wieland Hoffmann <themineo@gmail.com>
2017-04-17 15:47:37 +0200

    abbreviate_artistsort: Add enough conversion specifiers to a warning message
    Without this, the plugin doesn't work.

--- plugins/abbreviate_artistsort/abbreviate_artistsort.py
+++ plugins/abbreviate_artistsort/abbreviate_artistsort.py
@@ -157,7 +157,7 @@ def abbreviate_artistsort(tagger, metadata, track, release):
                         unsortTag,
                         unsort[i],
                     )
-                    log.warning("  Could not match surname (%s) in remaining unsorted:" % (surname, unsort))
+                    log.warning("  Could not match surname (%s) in remaining unsorted: %s" % (surname, unsort))
                     break
 
                 # Sorted:   Surname, Forename(s)...
