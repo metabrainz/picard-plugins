29b41ffb6a003bac993d59b94bb07634931f337e
Sambhav Kothari <sambhavs.email@gmail.com>
2017-05-04 12:37:50 +0530

    Port playlist plugin to 2.0

--- plugins/playlist/playlist.py
+++ plugins/playlist/playlist.py
@@ -12,18 +12,18 @@
 # GNU General Public License for more details.
 
 PLUGIN_NAME = "Generate M3U playlist"
-PLUGIN_AUTHOR = "Francis Chin"
+PLUGIN_AUTHOR = "Francis Chin, Sambhav Kothari"
 PLUGIN_DESCRIPTION = """Generate an Extended M3U playlist (.m3u8 file, UTF8
 encoded text). Relative pathnames are used where audio files are in the same
 directory as the playlist, otherwise absolute (full) pathnames are used."""
-PLUGIN_VERSION = "0.3"
-PLUGIN_API_VERSIONS = ["1.0.0", "1.1.0", "1.2.0", "1.3.0"]
+PLUGIN_VERSION = "1.0"
+PLUGIN_API_VERSIONS = ["2.0"]
 PLUGIN_LICENSE = "GPL-2.0"
 PLUGIN_LICENSE_URL = "https://www.gnu.org/licenses/gpl-2.0.html"
 
 import os.path
 
-from PyQt4 import QtCore, QtGui
+from PyQt5 import QtCore, QtWidgets
 from picard import log
 from picard.const import VARIOUS_ARTISTS_ID
 from picard.util import find_existing_path, encode_filename
@@ -67,8 +67,8 @@ class Playlist(object):
             b_lines.append(header.encode("utf-8"))
         for entry in self.entries:
             for row in entry:
-                b_lines.append(unicode(row).encode("utf-8"))
-        with open(encode_filename(self.filename), "wt") as f:
+                b_lines.append(string_(row).encode("utf-8"))
+        with open(encode_filename(self.filename), "wb") as f:
             f.writelines(b_lines)
 
 
@@ -78,7 +78,7 @@ class GeneratePlaylist(BaseAction):
     def callback(self, objs):
         current_directory = (self.config.persist["current_directory"]
                              or QtCore.QDir.homePath())
-        current_directory = find_existing_path(unicode(current_directory))
+        current_directory = find_existing_path(string_(current_directory))
         # Default playlist filename set as "%albumartist% - %album%.m3u8",
         # except where "Various Artists" is suppressed
         if _debug_level > 1:
@@ -97,22 +97,22 @@ class GeneratePlaylist(BaseAction):
         if _debug_level > 1:
             log.debug("{}: default playlist filename sanitized to {}".format(
                     PLUGIN_NAME, default_filename))
-        b_filename, b_selected_format = QtGui.QFileDialog.getSaveFileNameAndFilter(
+        b_filename, b_selected_format = QtWidgets.QFileDialog.getSaveFileName(
             None, "Save new playlist",
             os.path.join(current_directory, default_filename),
             "Playlist (*.m3u8 *.m3u)"
         )
         if b_filename:
-            filename = unicode(b_filename)
+            filename = string_(b_filename)
             playlist = Playlist(filename)
             playlist.add_header("#EXTM3U")
-            
+
             for album in objs:
                 for track in album.tracks:
                     if track.linked_files:
                         entry = PlaylistEntry(playlist, len(playlist.entries))
                         playlist.entries.append(entry)
-                        
+
                         # M3U EXTINF row
                         track_length_seconds = int(round(track.metadata.length / 1000.0))
                         # EXTINF format assumed to be fixed as follows:
@@ -122,7 +122,7 @@ class GeneratePlaylist(BaseAction):
                             title=track.metadata["title"]
                             )
                         )
-                        
+
                         # M3U URL row - assumes only one file per track
                         audio_filename = track.linked_files[0].filename
                         if _debug_level > 1:
@@ -136,8 +136,8 @@ class GeneratePlaylist(BaseAction):
                                     PLUGIN_NAME, audio_filename, os.path.dirname(filename)))
                         if os.path.dirname(filename) == os.path.dirname(audio_filename):
                             audio_filename = os.path.basename(audio_filename)
-                        entry.add(unicode(audio_filename))
-            
+                        entry.add(string_(audio_filename))
+
             playlist.write()
 
 
