747ef5087a2db767ffab4abca6cef508ce3e8dca
Sambhav Kothari <sambhavs.email@gmail.com>
2017-05-06 12:40:02 +0530

    Port remove perfect albums plugin to 2.0

--- plugins/remove_perfect_albums/remove_perfect_albums.py
+++ plugins/remove_perfect_albums/remove_perfect_albums.py
@@ -2,7 +2,7 @@ PLUGIN_NAME = 'Remove Perfect Albums'
 PLUGIN_AUTHOR = 'ichneumon, hrglgrmpf'
 PLUGIN_DESCRIPTION = '''Remove all perfectly matched albums from the selection.'''
 PLUGIN_VERSION = '0.2'
-PLUGIN_API_VERSIONS = ['0.15']
+PLUGIN_API_VERSIONS = ['0.15', '2.0']
 PLUGIN_LICENSE = "GPL-2.0"
 PLUGIN_LICENSE_URL = "https://www.gnu.org/licenses/gpl-2.0.html"
 
@@ -14,9 +14,9 @@ class RemovePerfectAlbums(BaseAction):
 
     def callback(self, objs):
         for album in objs:
-            if isinstance(album, Album) and album.is_complete() and album.get_num_unmatched_files() == 0\
-              and album.get_num_matched_tracks() == len(list(album.iterfiles()))\
-              and album.get_num_unsaved_files() == 0 and album.loaded == True:
+            if (isinstance(album, Album) and album.is_complete() and album.get_num_unmatched_files() == 0
+              	and album.get_num_matched_tracks() == len(list(album.iterfiles()))
+              	and album.get_num_unsaved_files() == 0 and album.loaded == True):
                 self.tagger.remove_album(album)
 
 register_album_action(RemovePerfectAlbums())
