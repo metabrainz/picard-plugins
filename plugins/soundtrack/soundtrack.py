b899de105788e3bbe9b61ec120885f056ec5e8fe
Sambhav Kothari <sambhavs.email@gmail.com>
2017-05-08 12:48:24 +0530

    Port soundtrack plugin to 2.0

--- plugins/soundtrack/soundtrack.py
+++ plugins/soundtrack/soundtrack.py
@@ -11,7 +11,7 @@ PLUGIN_LICENSE = 'WTFPL'
 PLUGIN_LICENSE_URL = 'http://www.wtfpl.net/'
 PLUGIN_DESCRIPTION = '''Sets the albumartist to "Soundtrack" if releasetype is a soundtrack.'''
 PLUGIN_VERSION = "0.1"
-PLUGIN_API_VERSIONS = ["1.0"]
+PLUGIN_API_VERSIONS = ["1.0", "2.0"]
 
 from picard.metadata import register_album_metadata_processor
 
