cfd370d35d6ffe8139e0a9eae6dcea2e80165e46
Bob Swift <bswift@rsds.ca>
2017-06-08 18:15:05 -0600

    Update to v0.05
    Use the _albumartists values when creating either the _aaeStd* or the
    _aaeCred* variables depending on the value of the 'Use standardized
    artist name' setting.
    
    Use the _albumartists_sort values when creating the _aaeSort* variables.

--- plugins/albumartistextension/albumartistextension.py
+++ plugins/albumartistextension/albumartistextension.py
@@ -23,13 +23,14 @@ PLEASE NOTE: Tagger scripts are required to make use of these hidden
 variables.
 '''
 
-PLUGIN_VERSION = "0.4"
+PLUGIN_VERSION = "0.5"
 PLUGIN_API_VERSIONS = ["1.4"]
 PLUGIN_LICENSE = "GPL-2.0 or later"
 PLUGIN_LICENSE_URL = "https://www.gnu.org/licenses/gpl-2.0.html"
 
-from picard import log
+from picard import config, log
 from picard.metadata import register_album_metadata_processor
+from picard.plugin import PluginPriority
 
 class AlbumArtistStdName:
 
@@ -43,6 +44,9 @@ class AlbumArtistStdName:
             stdArtist = ""
             sortArtist = ""
             aCount = 0
+            # Get the current lists of _albumartists and _albumartists_sort
+            metaAlbumArtists = dict.get(album_metadata,"~albumartists")
+            metaAlbumArtists_Sort = dict.get(album_metadata,"~albumartists_sort")
             # The 'name_credit' child should always be there.
             # This check is to avoid a runtime error if it doesn't exist for some reason.
             if 'name_credit' in releaseXmlNode.artist_credit[0].children:
@@ -55,10 +59,16 @@ class AlbumArtistStdName:
                     # Check if there is a 'joinphrase' specified.
                     if 'joinphrase' in ncredit.attribs:
                         tempPhrase = ncredit.joinphrase
-                    # Check if there is a 'name' specified.  This will be the
-                    # credited name.
-                    if 'name' in ncredit.children:
-                        tempCredName = ncredit.name[0].text
+                    # Set the credit name from the AlbumArtist list if the
+                    # 'Use standardized artist name' option is not checked
+                    # in Picard, otherwise use the XML information.
+                    if config.setting["standardize_artists"]:
+                        # Check if there is a 'name' specified.  This will be the
+                        # credited name.
+                        if 'name' in ncredit.children:
+                            tempCredName = ncredit.name[0].text
+                    else:
+                        tempCredName = metaAlbumArtists[aCount]
                     # The 'artist' child should always be there.
                     # This check is to avoid a runtime error if it doesn't
                     # exist for some reason.
@@ -67,7 +77,11 @@ class AlbumArtistStdName:
                         # This check is to avoid a runtime error if it
                         # doesn't exist for some reason.
                         if 'name' in ncredit.artist[0].children:
-                            tempStdName = ncredit.artist[0].name[0].text
+                            # Set the standardized name from the  AlbumArtist
+                            # list if the 'Use standardized artist name'
+                            # option is checked in Picard, otherwise use the
+                            # XML information.
+                            tempStdName = metaAlbumArtists[aCount] if config.setting["standardize_artists"] else ncredit.artist[0].name[0].text
                             stdArtist += tempStdName + tempPhrase
                             tCredName = tempCredName if len(tempCredName) > 0 else tempStdName
                             credArtist += tCredName + tempPhrase
@@ -77,17 +91,12 @@ class AlbumArtistStdName:
                         else:
                             log.error("%s: %r: Missing artist 'name' in XML contents: %s",
                                     PLUGIN_NAME, albumid, releaseXmlNode)
-                        # The 'sort_name' child should always be there.
-                        # This check is to avoid a runtime error if it
-                        # doesn't exist for some reason.
-                        if 'sort_name' in ncredit.artist[0].children:
-                            tempSortName = ncredit.artist[0].sort_name[0].text
-                            sortArtist += tempSortName + tempPhrase
-                            if aCount < 1:
-                                album_metadata["~aaeSortPrimaryAlbumArtist"] = tempSortName
-                        else:
-                            log.error("%s: %r: Missing artist 'sort_name' in XML contents: %s",
-                                    PLUGIN_NAME, albumid, releaseXmlNode)
+                        # Get the artist sort name from the
+                        # _albumartists_sort list
+                        tempSortName = metaAlbumArtists_Sort[aCount]
+                        sortArtist += tempSortName + tempPhrase
+                        if aCount < 1:
+                            album_metadata["~aaeSortPrimaryAlbumArtist"] = tempSortName
                     else:
                         log.error("%s: %r: Missing 'artist' in XML contents: %s",
                                 PLUGIN_NAME, albumid, releaseXmlNode)
@@ -108,4 +117,8 @@ class AlbumArtistStdName:
                       PLUGIN_NAME, albumid, releaseXmlNode)
         return None
 
-register_album_metadata_processor(AlbumArtistStdName().add_artist_std_name)
+# Register the plugin to run at a LOW priority so that other plugins that
+# modify the contents of the _albumartists and _albumartists_sort lists can
+# complete their processing and this plugin is working with the latest
+# updated data.
+register_album_metadata_processor(AlbumArtistStdName().add_artist_std_name, priority=PluginPriority.LOW)
