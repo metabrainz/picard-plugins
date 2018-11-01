8ff8e984c807d86e6074c2c664e34a7987bbef40
Wieland Hoffmann <themineo@gmail.com>
2018-11-01 12:22:00 +0100

    Run doctests in test.py

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import doctest
 import os
 import glob
 import json
@@ -87,6 +88,10 @@ class GenerateTestCase(unittest.TestCase):
             self.assertIsInstance(data['version'], str)
 
 
+def load_tests(loader, tests, ignore):
+    from plugins.addrelease import addrelease
+    tests.addTests(doctest.DocTestSuite(addrelease))
+    return tests
 
 
 if __name__ == '__main__':
