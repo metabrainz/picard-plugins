9d8b6b3018792f4f06f29ead33ef99c5dc4ed6d5
Philipp Wolfer <phil@parolu.io>
2018-11-27 07:52:49 +0100

    More GPL 2 or later fixes
    This is not a license change, the affected plugins always stated this license in the headers. For a long time the PLUGIN_LICENSE was not set this specific.

--- plugins/reorder_sides/reorder_sides.py
+++ plugins/reorder_sides/reorder_sides.py
@@ -28,9 +28,9 @@ PLUGIN_DESCRIPTION = """\
   changers
   (https://en.wikipedia.org/wiki/Record_changer#Automatic_sequencing)
   play in the correct order."""
-PLUGIN_VERSION = '1.0'
+PLUGIN_VERSION = '1.1'
 PLUGIN_API_VERSIONS = ['2.0']
-PLUGIN_LICENSE = 'GPL-3.0'
+PLUGIN_LICENSE = 'GPL-3.0-or-later'
 PLUGIN_LICENSE_URL = 'https://www.gnu.org/licenses/gpl-3.0.html'
 
 import collections
