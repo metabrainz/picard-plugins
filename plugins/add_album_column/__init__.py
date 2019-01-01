39a121f77169a4a58e233d3998b93c5bd805640b
evandrocoan <evandrocoan@hotmail.com>
2019-01-01 11:57:18 -0200

    Added WARNING: This plugin cannot be disabled #195

--- plugins/add_album_column/__init__.py
+++ plugins/add_album_column/__init__.py
@@ -36,7 +36,11 @@
 
 PLUGIN_NAME = u"Add Album Column"
 PLUGIN_AUTHOR = u"Evandro Coan"
-PLUGIN_DESCRIPTION = "Add the Album column to the main window panel."
+PLUGIN_DESCRIPTION = """Add the Album column to the main window panel.
+
+WARNING: This plugin cannot be disabled. See:
+https://github.com/metabrainz/picard-plugins/pull/195
+"""
 
 PLUGIN_VERSION = "1.0"
 PLUGIN_API_VERSIONS = ["2.0"]
