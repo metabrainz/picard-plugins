9238a9aaf28aca92c1401e76f957a7e8731acb5d
Philipp Wolfer <ph.wolfer@gmail.com>
2020-01-29 12:05:09 +0100

    remmove_perfect_albums: Keep UI responsive

--- plugins/remove_perfect_albums/remove_perfect_albums.py
+++ plugins/remove_perfect_albums/remove_perfect_albums.py
@@ -1,10 +1,12 @@
 PLUGIN_NAME = 'Remove Perfect Albums'
 PLUGIN_AUTHOR = 'ichneumon, hrglgrmpf'
 PLUGIN_DESCRIPTION = '''Remove all perfectly matched albums from the selection.'''
-PLUGIN_VERSION = '0.2'
-PLUGIN_API_VERSIONS = ['0.15', '2.0']
+PLUGIN_VERSION = '0.3'
+PLUGIN_API_VERSIONS = ['2.0', '2.1', '2.2', '2.3']
 PLUGIN_LICENSE = "GPL-2.0"
 PLUGIN_LICENSE_URL = "https://www.gnu.org/licenses/gpl-2.0.html"
+
+from PyQt5.QtCore import QCoreApplication
 
 from picard.album import Album
 from picard.ui.itemviews import BaseAction, register_album_action
@@ -17,5 +19,6 @@ class RemovePerfectAlbums(BaseAction):
             if (isinstance(album, Album) and album.loaded and album.is_complete()
               	and album.get_num_unsaved_files() == 0):
                 self.tagger.remove_album(album)
+            QCoreApplication.processEvents()
 
 register_album_action(RemovePerfectAlbums())
