766c01071f44f84265eb1658bd0a8b8f679ed8f4
Philipp Wolfer <ph.wolfer@gmail.com>
2020-03-03 09:29:18 +0100

    theaudiodb: Added note about contributing to plugin settings

--- plugins/theaudiodb/ui_options_theaudiodb.py
+++ plugins/theaudiodb/ui_options_theaudiodb.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'options_theaduiodb.ui'
+# Form implementation generated from reading ui file 'options_theaudiodb.ui'
 #
-# Created by: PyQt5 UI code generator 5.12
+# Created by: PyQt5 UI code generator 5.14.1
 #
 # WARNING! All changes made in this file will be lost!
+
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
@@ -46,8 +47,6 @@ class Ui_TheAudioDbOptionsPage(object):
         self.vboxlayout.addWidget(self.verticalGroupBox)
         spacerItem = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
         self.vboxlayout.addItem(spacerItem)
-        spacerItem1 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
-        self.vboxlayout.addItem(spacerItem1)
 
         self.retranslateUi(TheAudioDbOptionsPage)
         QtCore.QMetaObject.connectSlotsByName(TheAudioDbOptionsPage)
@@ -59,10 +58,8 @@ class Ui_TheAudioDbOptionsPage(object):
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'Cantarell\'; font-size:10pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">This plugin loads cover art from <a href=\"https://www.theaudiodb.com\"><span style=\" text-decoration: underline; color:#0000ff;\">TheAudioDB</span></a>. If you want to improve the results of this plugin please contribute.</p></body></html>"))
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">This plugin loads cover art from <a href=\"https://www.theaudiodb.com\"><span style=\" text-decoration: underline; color:#1b6acb;\">TheAudioDB</span></a>. TheAudioDB is a community Database of audio artwork and data. Their content is only possible thanks to the hard work of volunteer editors. If you like this plugin, please consider <a href=\"https://www.theaudiodb.com/forum-new/ucp.php?mode=register\"><span style=\" text-decoration: underline; color:#1b6acb;\">registering</span></a> as an editor or supporting the TheAudioDB <a href=\"https://www.patreon.com/thedatadb\"><span style=\" text-decoration: underline; color:#1b6acb;\">patreon campaign</span></a>.</p></body></html>"))
         self.verticalGroupBox.setTitle(_translate("TheAudioDbOptionsPage", "Medium images"))
         self.theaudiodb_cdart_use_always.setText(_translate("TheAudioDbOptionsPage", "Always load medium images"))
         self.theaudiodb_cdart_use_if_no_albumcover.setText(_translate("TheAudioDbOptionsPage", "Load only if no front cover is available"))
         self.theaudiodb_cdart_use_never.setText(_translate("TheAudioDbOptionsPage", "Never load medium images"))
-
-
