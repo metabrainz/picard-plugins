9f7aea6064468ca269a4d674909266c7e735b610
Wieland Hoffmann <themineo@gmail.com>
2020-04-22 22:02:16 +0200

    keep: Make it possible to keep tags with descriptions without enumerating all possible ones
    This allows keeping tags with descriptions like performer:, lyrics: and
    comment:. We simply check if the part of the tag name before the separator (so
    `performer`) should be kept.
    
    This still allows keeping only tags with specific descriptions.

--- plugins/keep/keep.py
+++ plugins/keep/keep.py
@@ -3,9 +3,13 @@ PLUGIN_AUTHOR = "Wieland Hoffmann"
 PLUGIN_DESCRIPTION = """
 Adds a $keep() function to delete all tags except the ones that you want.
 Tags beginning with `musicbrainz_` are kept automatically, as are tags
-beginning with `_`."""
+beginning with `_`.
 
-PLUGIN_VERSION = "1.1"
+To keep all tags that can have a description (like `comment`, lyrics` and
+`performer`), add `<tagname without description>` (not including `:`) to the
+list of tags to keep."""
+
+PLUGIN_VERSION = "1.2"
 PLUGIN_API_VERSIONS = ["0.15.0", "0.15.1", "0.16.0", "1.0.0", "1.1.0", "1.2.0",
                        "1.3.0", "2.0"]
 PLUGIN_LICENSE = "GPL-2.0-or-later"
@@ -24,8 +28,13 @@ def transltag(tag):
 def keep(parser, *keeptags):
     tags = list(parser.context.keys())
     for tag in tags:
-        if (transltag(tag) not in keeptags and
-            not tag.startswith("musicbrainz_") and
-                not tag.startswith("~")):
-            parser.context.pop(tag, None)
+        if (transltag(tag) in keeptags or
+            tag.startswith("musicbrainz_") or
+            tag.startswith("~")):
+            continue
+        if ":" in tag:
+            tag_without_description = tag.split(":")[0]
+            if tag_without_description in keeptags:
+                continue
+        parser.context.pop(tag, None)
     return ""
