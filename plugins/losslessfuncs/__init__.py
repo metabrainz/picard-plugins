d4e98261fd30753d77d157c16c46ec4ff5da878a
Philipp Wolfer <ph.wolfer@gmail.com>
2021-03-01 17:18:58 +0100

    losslessfuncs: Renamed formathelpers to losslessfuncs, added AIFF support

--- plugins/losslessfuncs/__init__.py
+++ plugins/losslessfuncs/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2014 Philipp Wolfer
+# Copyright (C) 2014, 2017, 2021 Philipp Wolfer
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -16,29 +16,32 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-PLUGIN_NAME = 'Tagger script format helper functions'
+PLUGIN_NAME = 'Tagger script functions $is_lossless() and $is_lossy()'
 PLUGIN_AUTHOR = 'Philipp Wolfer'
-PLUGIN_DESCRIPTION = 'A collection of useful functions to deal with file formats.'
-PLUGIN_VERSION = "0.1"
-PLUGIN_API_VERSIONS = ["1.3.0", "2.0"]
+PLUGIN_DESCRIPTION = 'Tagger script functions to detect if a file is lossless or lossy'
+PLUGIN_VERSION = "0.2"
+PLUGIN_API_VERSIONS = ["2.0", "2.1", "2.2", "2.3", "2.4", "2.5", "2.6"]
+PLUGIN_LICENSE = "GPL-2.0"
+PLUGIN_LICENSE_URL = "https://www.gnu.org/licenses/gpl-2.0.html"
+
+
+LOSSLESS_EXTENSIONS = ['flac', 'oggflac', 'ape', 'ofr', 'tak', 'wv', 'tta',
+                       'aiff', 'aif', 'wav']
 
 from picard.script import register_script_function
-
-LOSSLESS_EXTENSIONS = ['flac', 'oggflac', 'ape', 'ofr', 'tak', 'wv', 'tta']
 
 
 def is_lossless(parser):
     """
     Returns true, if the file processed is a lossless audio format.
-    Note: This depends mainly on the file extension and does not look inside
-    the file to detect the codec.
+    Note: This depends mainly on the file extension and does not look inside the
+    file to detect the codec.
     """
     if parser.context['~extension'] in LOSSLESS_EXTENSIONS:
         return "1"
     elif parser.context['~extension'] == 'm4a':
-        # Mutagen < 1.26 fails to detect the bitrate
-        # for Apple Lossless Audio Codec.
-        if not parser.context['~bitrate'] or float(parser.context['~bitrate']) > 1000:
+        # Mutagen < 1.26 fails to detect the bitrate for Apple Lossless Audio Codec.
+        if not parser.context['~bitrate'] or parser.context['~bitrate'] > 1000:
             return "1"
         else:
             return ""
