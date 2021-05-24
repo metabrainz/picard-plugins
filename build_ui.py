da8c37424b12723569debe8453a990c39090811c
Philipp Wolfer <ph.wolfer@gmail.com>
2021-05-24 10:46:57 +0200

    Added script to build .ui files

---
+++ build_ui.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import glob
+import os
+
+from PyQt5 import uic
+
+
+plugin_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'plugins')
+
+
+def compile_ui(uifile, pyfile):
+    print("compiling %s -> %s" % (uifile, pyfile))
+    with open(pyfile, "w") as out:
+        uic.compileUi(uifile, out)
+
+
+if __name__ == '__main__':
+    for uifile in glob.glob(os.path.join(plugin_dir, '*/*.ui')):
+        pyfile = os.path.splitext(os.path.basename(uifile))[0] + '.py'
+        pyfile = os.path.join(os.path.dirname(uifile), pyfile)
+        compile_ui(uifile, pyfile)
