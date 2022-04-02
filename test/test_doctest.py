8af73d41c8bcca02b8e4ad1c301b5f98d521f2d6
Bob Swift <bswift@rsds.ca>
2022-04-01 23:29:37 -0600

    Add Key Wheel Converter plugin

--- test/test_doctest.py
+++ test/test_doctest.py
@@ -8,4 +8,6 @@ def load_tests(loader, tests, ignore):
     tests.addTests(doctest.DocTestSuite(decade))
     from plugins.standardise_feat import standardise_feat
     tests.addTests(doctest.DocTestSuite(standardise_feat))
+    from plugins.key_wheel_converter import key_wheel_converter
+    tests.addTests(doctest.DocTestSuite(key_wheel_converter))
     return tests
