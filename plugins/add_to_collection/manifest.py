c773e0cf2d6bd8dbbfab70f2d0c9d779a99c523d
Philipp Wolfer <ph.wolfer@gmail.com>
2024-03-19 08:16:11 +0100

    add_to_collection: Fixed user guide URL

--- plugins/add_to_collection/manifest.py
+++ plugins/add_to_collection/manifest.py
@@ -1,10 +1,10 @@
 PLUGIN_NAME = "Add to Collection"
 PLUGIN_AUTHOR = "Dvir Yitzchaki (dvirtz@gmail.com)"
 PLUGIN_DESCRIPTION = "Adds any saved release to one of your user collections"
-PLUGIN_VERSION = "0.1"
+PLUGIN_VERSION = "0.1.1"
 PLUGIN_API_VERSIONS = ["2.0"]
 PLUGIN_LICENSE = ("MIT",)
 PLUGIN_LICENSE_URL = "https://spdx.org/licenses/MIT.html"
 PLUGIN_USER_GUIDE_URL = (
-    "https://github.com/metabrainz/picard-plugins/plugins/add-to-collection/README.md"
+    "https://github.com/metabrainz/picard-plugins/blob/2.0/plugins/add_to_collection/README.md"
 )
