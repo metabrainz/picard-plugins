844fbaacc290c3decd2ad8a7c8782ec549600495
nrth3rnlb <206222+nrth3rnlb@users.noreply.github.com>
2025-11-15 17:57:52 +0100

    feat: Update Title Cleaner OST plugin to v1.3.3
    - chore(init): bump PLUGIN_VERSION to 1.3.3 (4c38dd5)
    - fix(init): remove __version__ and set PLUGIN_VERSION directly (905a4d5)
    - 1.3.2 (4524cff)
    - fix(logic): Changes to the title will also be made if no checkbox is selected (4347de0)
    - fix(logging): add debug log for title_cleaner_ost function (e422cd9)

--- plugins/title_cleaner_ost/__init__.py
+++ plugins/title_cleaner_ost/__init__.py
@@ -5,8 +5,6 @@ Title Cleaner OST Plugin for MusicBrainz Picard.
 Removes soundtrack-related information from album titles using regex.
 """
 
-__version__ = "1.3.2"
-
 from PyQt5.QtWidgets import QCheckBox
 
 PLUGIN_NAME = "Title Cleaner OST"
@@ -18,7 +16,7 @@ Regular expressions are a powerful tool. They can therefore also cause serious d
 Use https://regex101.com/ to test your pattern.
 Use at your own risk.
 """
-PLUGIN_VERSION = __version__
+PLUGIN_VERSION = "1.3.3"
 PLUGIN_API_VERSIONS = ["2.7", "2.8"]
 PLUGIN_LICENSE = "GPL-2.0"
 PLUGIN_LICENSE_URL = "https://www.gnu.org/licenses/gpl-2.0.html"
