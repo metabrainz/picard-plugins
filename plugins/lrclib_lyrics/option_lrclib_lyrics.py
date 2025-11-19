b65070bd921a3c2103244c0e700159b55065774c
Andreas Böttger <206222+nrth3rnlb@users.noreply.github.com>
2025-11-19 19:16:55 +0100

    Update comment in option_lrclib_lyrics.py

--- plugins/lrclib_lyrics/option_lrclib_lyrics.py
+++ plugins/lrclib_lyrics/option_lrclib_lyrics.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file '/home/andreas/Projects/picard-plugins/plugins/lrclib_lyrics/option_lrclib_lyrics.ui'
+# Form implementation generated from reading ui file 'option_lrclib_lyrics.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.10
 #
